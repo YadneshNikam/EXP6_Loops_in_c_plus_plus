3259774e3b34a056410a43e91b49d66596613ad1
YadneshNikam <yadneshnikam32@gmail.com>
2025-07-23 18:48:20 +0530

    Update Exp6_password.cpp

--- Exp6_password.cpp
+++ Exp6_password.cpp
@@ -22,3 +22,20 @@ int main(){
     return 0;
     
 }
+/*Enter a password: qwertz
+Password accepted!
+Enter a password: eeeyg
+Incorrect password. Try again: 
+Hint: The password is the first five letters of your keyboard.
+Enter a password: dgdfg
+Incorrect password. Try again: 
+Hint: The password is the first five letters of your keyboard.
+Enter a password: fdgfd
+Incorrect password. Try again: 
+Hint: The password is the first five letters of your keyboard.
+Enter a password: gfdg
+Incorrect password. Try again: 
+Hint: The password is the first five letters of your keyboard.
+Enter a password: fdgs
+You have exceeded the maximum number of attempts.
+The system will be locked for 1 minute*/
